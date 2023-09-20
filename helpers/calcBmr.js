0ea27d8ed10c4f09f3514e1655160c5f276b1d91
Alexander <yusovsky2@gmail.com>
2023-09-20 22:23:39 +0300

    Update calcBmr.js

--- helpers/calcBmr.js
+++ helpers/calcBmr.js
@@ -23,20 +23,26 @@ const calcBmr = ({ desiredWeight, height, birthdate, levelActivity, sex }) => {
     case 1:
       result =
         (10 * desiredWeight + 6.25 * height - 5 * years + sexValue) * 1.2;
+      break;
     case 2:
       result =
         (10 * desiredWeight + 6.25 * height - 5 * years + sexValue) * 1.375;
+      break;
     case 3:
       result =
         (10 * desiredWeight + 6.25 * height - 5 * years + sexValue) * 1.55;
+      break;
     case 4:
       result =
         (10 * desiredWeight + 6.25 * height - 5 * years + sexValue) * 1.725;
+      break;
     case 5:
       result =
         (10 * desiredWeight + 6.25 * height - 5 * years + sexValue) * 1.9;
+      break;
 
     default:
+      result = 1;
       break;
   }
 

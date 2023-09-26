98f71d81b46520124e7088890144a0ad6f699ffc
VladimirPopovich27 <117470833+VladimirPopovich27@users.noreply.github.com>
2023-09-26 22:22:42 +0200

    exercises

--- routes/api/exerciseCategories.js
+++ routes/api/exerciseCategories.js
@@ -1,8 +1,62 @@
-// const express = require('express');
-// const router = express.Router();
-// const { authenticate } = require('../../middelwares');
-// const ctrl = require('../../controllers/exerciseCategories');
+const { HttpError, ctrlWrapper } = require('../helpers');
+const mongoose = require('mongoose');
 
-// router.get('/:category', authenticate, ctrl.getExerciseCategories);
+const getExerciseCategories = async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  const { category } = req.params;
+  const skip = (page - 1) * limit;
 
-// module.exports = router;
+  const filterCategories = ['Body parts', 'Muscles', 'Equipment'];
+  let filterValue = '';
+
+  if (category) {
+    switch (category) {
+      case 'bodyparts':
+        filterValue = filterCategories[0];
+        break;
+      case 'muscles':
+        filterValue = filterCategories[1];
+        break;
+      case 'equipment':
+        filterValue = filterCategories[2];
+        break;
+
+      default:
+        throw HttpError(404, 'Category not found');
+    }
+  }
+
+  const findObj = {};
+  if (filterValue) {
+    findObj.filter = filterValue;
+  }
+
+  const Filters = mongoose.connection.collection('filters');
+
+  const aggregatePipeline = [
+    {
+      $match: findObj,
+    },
+    {
+      $facet: {
+        data: [{ $skip: skip }, { $limit: Number(limit) }],
+        totalCount: [{ $count: 'total' }],
+      },
+    },
+    {
+      $unwind: '$totalCount',
+    },
+  ];
+  const result = await Filters.aggregate(aggregatePipeline).toArray();
+  const data = result[0]?.data;
+  const totalCount = result[0]?.totalCount ? result[0]?.totalCount?.total : 0;
+
+  res.status(200).json({
+    totalCount,
+    exerciseCategories: data || [],
+  });
+};
+
+module.exports = {
+  getExerciseCategories: ctrlWrapper(getExerciseCategories),
+};
--- routes/api/exercises.js
+++ routes/api/exercises.js
@@ -1,8 +1,48 @@
-// const express = require('express');
-// const router = express.Router();
-// const { authenticate } = require('../../middelwares');
-// const ctrl = require('../../controllers/exercises');
+const { HttpError, ctrlWrapper } = require('../helpers');
+const mongoose = require('mongoose');
 
-// router.get('/', authenticate, ctrl.getExercises);
+const getExercises = async (req, res) => {
+  const { page = 1, limit = 10, bodyPart, equipment, target } = req.query;
+  const skip = (page - 1) * limit;
 
-// module.exports = router;
+  const filter = {};
+
+  if (bodyPart) {
+    filter.bodyPart = bodyPart;
+  }
+  if (equipment) {
+    filter.equipment = equipment;
+  }
+  if (target) {
+    filter.target = target;
+  }
+
+  const aggregatePipeline = [
+    {
+      $match: filter,
+    },
+    {
+      $facet: {
+        data: [{ $skip: skip }, { $limit: Number(limit) }],
+        totalCount: [{ $count: 'total' }],
+      },
+    },
+    {
+      $unwind: '$totalCount',
+    },
+  ];
+
+  const Exercises = mongoose.connection.collection('exercises');
+  const result = await Exercises.aggregate(aggregatePipeline).toArray();
+  const data = result[0]?.data;
+  const totalCount = result[0]?.totalCount ? result[0]?.totalCount?.total : 0;
+
+  res.status(200).json({
+    totalCount,
+    exercises: data || [],
+  });
+};
+
+module.exports = {
+  getExercises: ctrlWrapper(getExercises),
+};
